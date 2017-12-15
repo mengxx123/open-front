5ab349c2461792fb0a7260c110a5f432ca96464e
gitcjh <1418503647@.qq.com>
2017-12-15 15:10:43 +0800

    first commit

---
+++ src/components/header/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="page-header">
+        <div class="container">
+            <div>云设开放平台</div>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+            }
+        },
+        mounted() {
+        },
+        methods: {
+        }
+    }
+</script>
